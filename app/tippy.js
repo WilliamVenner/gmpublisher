c75c3e9170fb9eba34c79995cda0d8ab5470ec33
William Venner <william@venner.io>
2021-03-16 17:07:51 +0000

    Stop tippy if content is falsy

--- app/tippy.js
+++ app/tippy.js
@@ -2,10 +2,12 @@ import { default as tippyJS, followCursor } from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 
 export function tippy(node, content) {
+	if (!content) return;
 	tippyJS(node, { content });
 };
 
 export function tippyFollow(node, content) {
+	if (!content) return;
 	tippyJS(node, {
 		content,
 		followCursor: true,
