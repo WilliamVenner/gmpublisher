153535471ffee62340b549cb6820c9f8a3f644b0
William Venner <william@venner.io>
2021-03-31 01:54:28 +0100

    Add appdata

---
+++ src-tauri/src/appdata.rs
@@ -0,0 +1,116 @@
+use std::{fs::File, io::{BufReader, BufWriter}, path::PathBuf};
+
+use lazy_static::lazy_static;
+use parking_lot::RwLock;
+use tauri::WebviewManager;
+use serde::{Serialize, Deserialize};
+
+const APP_INFO: app_dirs::AppInfo = app_dirs::AppInfo {
+	name: "gmpublisher",
+	author: "WilliamVenner"
+};
+lazy_static! {
+	static ref APP_DATA_DIR: PathBuf = app_dirs::app_root(app_dirs::AppDataType::UserConfig, &APP_INFO).unwrap();
+	static ref APP_SETTINGS_PATH: PathBuf = app_dirs::app_root(app_dirs::AppDataType::UserConfig, &APP_INFO).unwrap().join("settings.json");
+}
+
+fn settings_path() -> PathBuf {
+	APP_SETTINGS_PATH.to_owned()
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct Settings {
+	pub(crate) gmod: Option<PathBuf>,
+	pub(crate) window_size: (f64, f64),
+	pub(crate) window_maximized: bool,
+	pub(crate) destinations: Vec<PathBuf>,
+	pub(crate) create_folder_on_extract: bool,
+}
+impl Default for Settings {
+    fn default() -> Self {
+        Self {
+			gmod: None,
+			window_size: (800., 600.),
+			window_maximized: true,
+			destinations: Vec::new(),
+			create_folder_on_extract: true,
+		}
+    }
+}
+impl Settings {
+	pub(crate) fn init() -> Settings {
+		match Settings::load() {
+			Ok(settings) => settings,
+			Err(_) => Settings::default()
+		}
+	}
+
+	fn load() -> Result<Settings, anyhow::Error> {
+		Ok(serde_json::de::from_reader(BufReader::new(File::open(&*APP_SETTINGS_PATH)?))?)
+	}
+
+	pub(crate) fn save(&self) -> Result<(), anyhow::Error> {
+		Ok(serde_json::ser::to_writer(BufWriter::new(File::open(&*APP_SETTINGS_PATH)?), self)?)
+	}
+
+	pub(crate) fn send(&self) {
+		todo!()
+	}
+
+	pub(crate) fn sanitize(&mut self) -> bool {
+		// TODO replace with drain_filter when it's stable
+		let mut i = 0;
+		while i != self.destinations.len() {
+			if !self.destinations[i].exists() {
+				self.destinations.remove(i);
+			} else {
+				i += 1;
+			}
+		}
+
+		self.destinations.truncate(20);
+
+		true
+	}
+}
+
+#[derive(Debug, Serialize)]
+pub(crate) struct AppData {
+	pub(crate) settings: RwLock<Settings>,
+}
+impl AppData {
+	pub(crate) fn init() -> Self {
+		Self {
+			settings: RwLock::new(Settings::init())
+		}
+	}
+}
+
+#[tauri::command(with_manager)]
+pub(crate) fn update_settings(mgr: WebviewManager, mut settings: Settings) {
+	if settings.sanitize() {
+		settings.save().ok();
+		*crate::APP_DATA.settings.write() = settings;
+	}
+}
+
+pub(crate) struct Plugin;
+impl<Application: tauri::ApplicationExt + 'static> tauri::plugin::Plugin<Application> for Plugin {
+	fn initialization_script(&self) -> Option<String> {
+		Some(
+			include_str!("../../app/plugins/AppData.js")
+				.replace(
+					"{$_SETTINGS_$}",
+					&serde_json::ser::to_string(&*crate::APP_DATA).unwrap().replace("\\", "\\\\").replace("'", "\\'"),
+				)
+				.replace(
+					"{$_WS_DEAD_$}",
+					&serde_json::ser::to_string(&crate::WorkshopItem::from(steamworks::PublishedFileId(0))).unwrap().replace("\\", "\\\\").replace("'", "\\'"),
+				),
+		)
+	}
+
+	fn name(&self) -> &'static str {
+        "AppData"
+    }
+}
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -18,15 +18,32 @@ lazy_static! {
 	static ref GMA: octopus::GMA = octopus::GMA::init();
 }
 
+pub(crate) mod appdata;
+pub(crate) use appdata::AppData;
+lazy_static! {
+	pub(crate) static ref APP_DATA: AppData = AppData::init();
+}
+
+pub(crate) use octopus::steamworks::WorkshopItem;
+
 fn main() {
+	lazy_static::initialize(&APP_DATA);
 	lazy_static::initialize(&STEAMWORKS);
 	lazy_static::initialize(&GMA);
 
 	tauri::AppBuilder::default()
 		.create_webview("gmpublisher".to_string(), tauri::WindowUrl::App, |args| {
-			Ok(args.title("gmpublisher".to_string()).resizable(true).width(800.).height(600.))
+			let settings = APP_DATA.settings.read();
+			Ok(
+				args.title(format!("gmpublisher v{}", env!("CARGO_PKG_VERSION")))
+				.resizable(true)
+				.width(settings.window_size.0)
+				.height(settings.window_size.1)
+				//.maximized(settings.window_maximized)
+			)
 		})
 		.unwrap()
+		.plugin(appdata::Plugin)
 		.invoke_handler(tauri::generate_handler![])
 		.build(tauri::generate_context!())
 		.run();
