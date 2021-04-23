29d673d76e9b8f42a3dbcc9e208342e6c558d36c
William Venner <william@venner.io>
2021-04-23 04:21:53 +0100

    Search almost done

--- app/components/Search.svelte
+++ app/components/Search.svelte
@@ -3,8 +3,7 @@
 	import { Search, Cross } from 'akar-icons-svelte';
 	import { invoke } from '@tauri-apps/api/tauri';
 	import { Transaction } from '../transactions';
-
-	let searchActive = false;
+	import Loading from './Loading.svelte';
 
 	function getRandomInt(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -22,26 +21,40 @@
 	}
 
 	let searchResults = [];
+	let hasMoreResults = false;
+	let isLoading = false;
+	let isSearching = false;
+	let searchInput;
 	function updateSearch() {
 		const query = this.value.trim();
 		if (query.length === 0) {
 			searchResults = [];
+			isLoading = false;
+			isSearching = false;
 		} else {
 			invoke('search', { salt: getSalt(), query: this.value });
+			if (!isSearching) isLoading = true;
+			isSearching = true;
 		}
 	}
 
 	function clearSearch() {
-
+		searchInput.value = '';
+		searchInput.focus();
+		searchResults = [];
+		isLoading = false;
+		isSearching = false;
 	}
 
 	invoke('search_channel').then(transaction_id => {
 		const transaction = new Transaction(transaction_id);
 		transaction.listen(event => {
 			if (event.stream) {
-				const [salt, results] = event.data;
+				const [salt, [results, hasMore]] = event.data;
 				if (salt !== prevSalt) return;
+				hasMoreResults = hasMore;
 				searchResults = results;
+				isLoading = false;
 			}
 		});
 	});
@@ -49,20 +62,37 @@
 
 <main class:results={searchResults.length > 0}>
 	<div id="input-container">
-		<input type="text" id="search" placeholder={$_('search')} on:input={updateSearch}/>
-		{#if searchActive}
+		<input type="text" id="search" placeholder={$_('search')} on:input={updateSearch} bind:this={searchInput}/>
+		{#if isSearching}
 			<span id="cancel-search" on:click={clearSearch}><Cross size="1rem"/></span>
 		{:else}
-			<Search size="1rem"/>
+			<Search id="search-icon" size="1rem"/>
 		{/if}
 	</div>
-	{#if searchResults.length > 0}
-		<div id="search-results">
+	<div id="search-results" class="hide-scroll" class:loading={isLoading}>
+		{#if isLoading}
+			<Loading size="1.5rem"/>
+		{:else if searchResults.length > 0}
 			{#each searchResults as result}
-				<div class="result">{result[0]}</div>
+				<div class="result">
+					<div>
+						{result[0]}
+						{#if result[1] && result[1].source === 'my_workshop'}
+							<a class="association" target="_blank" href="https://steamcommunity.com/sharedfiles/filedetails/?id={result[1].association}">https://steamcommunity.com/sharedfiles/filedetails/?id={result[1].association}</a>
+						{:else}
+							<div class="association">{result[1].association}</div>
+						{/if}
+					</div>
+					{#if result[1]}
+						<div>{$_(result[1].source)}</div>
+					{/if}
+				</div>
 			{/each}
-		</div>
-	{/if}
+			{#if hasMoreResults}
+				<div class="show-more">{$_('show_more')}</div>
+			{/if}
+		{/if}
+	</div>
 </main>
 
 <style>
@@ -111,8 +141,17 @@
 	main:focus-within > div {
 		width: 60%;
 	}
-	#search:focus + :global(.icon) {
+	#search:focus + :global(.icon), #search + #cancel-search > :global(.icon) {
 		opacity: 1;
+	}
+	#search + :global(#search-icon) {
+		pointer-events: none;
+	}
+	#cancel-search > :global(.icon) {
+		cursor: pointer;
+	}
+	#input-container {
+		z-index: 2;
 	}
 
 	main.results #search:focus {
@@ -126,10 +165,60 @@
 	main > #search-results {
 		position: absolute;
 		top: 100%;
-		padding: 1rem;
 		background: #474747;
+		box-shadow: 0 0 10px #0000009e;
+		z-index: 1;
+		max-height: calc(100vh - 70px - 1rem);
 	}
-	main > #search-results .result:not(:last-child) {
-		margin-bottom: .5rem;
+	main > #search-results .result {
+		display: flex;
+		padding: .7rem;
+		cursor: pointer;
+	}
+	main > #search-results .result, #search-results .show-more {
+		transition: background-color .1s;
+	}
+	main > #search-results .result:hover, #search-results .show-more:hover {
+		background-color: rgb(0,0,0,.2);
+	}
+	main > #search-results.show-more .result:last-child {
+		padding-bottom: 0;
+	}
+	main > #search-results .result > div:first-child {
+		flex: 1;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+	main > #search-results .result > div:last-child {
+		margin-left: .5rem;
+		opacity: .5;
+	}
+
+	#search-results .show-more {
+		padding: 1.25rem;
+		text-align: center;
+		color: #00adff;
+		font-weight: bold;
+		font-size: 1em;
+		cursor: pointer;
+	}
+
+	#search-results.loading {
+		padding: 1rem;
+	}
+
+	#search-results .association {
+		display: block;
+		opacity: .5;
+		font-size: .8em;
+		margin-top: .5rem;
+	}
+	#search-results a.association {
+		transition: opacity .25s, color .25s;
+	}
+	#search-results a.association:hover {
+		opacity: 1;
+		color: #46b0ff;
 	}
 </style>
--- src-tauri/src/search.rs
+++ src-tauri/src/search.rs
@@ -1,4 +1,4 @@
-use std::{cell::UnsafeCell, cmp::Ordering, path::PathBuf, sync::{Arc, atomic::{AtomicBool, AtomicU8}}};
+use std::{path::PathBuf, sync::{Arc, atomic::{AtomicBool, AtomicU8}}};
 
 use rayon::prelude::*;
 use steamworks::PublishedFileId;
@@ -10,62 +10,24 @@ use fuzzy_matcher::skim::SkimMatcherV2;
 
 use crate::{GMAFile, Transaction, WorkshopItem};
 
+const MAX_QUICK_RESULTS: u8 = 10;
+
 #[derive(Clone, Serialize, Debug)]
-pub enum SearchAssociation {
-	PublishedFileId(PublishedFileId),
-	Path(PathBuf)
+#[serde(rename_all = "snake_case")]
+#[serde(tag = "source", content = "association")]
+pub enum SearchItemSource {
+	InstalledAddons(PathBuf),
+	MyWorkshop(PublishedFileId),
+	WorkshopItem(PublishedFileId),
 }
-impl PartialOrd for SearchAssociation {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-		match self {
-			SearchAssociation::PublishedFileId(a) => match other {
-				SearchAssociation::PublishedFileId(b) => a.partial_cmp(b),
-				_ => None
-			},
-			SearchAssociation::Path(a) => match other {
-				SearchAssociation::Path(b) => a.partial_cmp(b),
-				_ => None
-			},
-		}
-    }
-}
-impl Ord for SearchAssociation {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-		match self {
-			SearchAssociation::PublishedFileId(a) => match other {
-				SearchAssociation::PublishedFileId(b) => a.cmp(b),
-				_ => Ordering::Less
-			},
-			SearchAssociation::Path(a) => match other {
-				SearchAssociation::Path(b) => a.cmp(b),
-				_ => Ordering::Less
-			},
-		}
-    }
-}
-impl PartialEq for SearchAssociation {
-    fn eq(&self, other: &Self) -> bool {
-		match self {
-			SearchAssociation::PublishedFileId(a) => match other {
-				SearchAssociation::PublishedFileId(b) => a == b,
-				_ => false
-			},
-			SearchAssociation::Path(a) => match other {
-				SearchAssociation::Path(b) => a == b,
-				_ => false
-			},
-		}
-    }
-}
-impl Eq for SearchAssociation {}
 
 #[derive(Debug)]
 pub struct SearchItem {
 	label: String,
 	terms: Vec<String>,
-	associations: Vec<SearchAssociation>,
 	timestamp: u64,
-	len: usize
+	len: usize,
+	source: SearchItemSource
 }
 impl PartialOrd for SearchItem {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
@@ -83,42 +45,31 @@ impl Ord for SearchItem {
 }
 impl PartialEq for SearchItem {
     fn eq(&self, other: &Self) -> bool {
-		if self.associations.len() == other.associations.len() {
-			if self.associations.len() == 1 {
-				self.associations[0] == other.associations[1]
-			} else {
-				let mut eq = true;
-				let mut i = 0;
-				while i < self.associations.len() {
-					if self.associations[i] != other.associations[i] {
-						eq = false;
-					}
-					i += 1;
-				}
-				eq
+		match &self.source {
+		    SearchItemSource::InstalledAddons(a) => match &other.source {
+				SearchItemSource::InstalledAddons(b) => a == b,
+				_ => false,
+			},
+		    SearchItemSource::MyWorkshop(a) => match &other.source {
+				SearchItemSource::MyWorkshop(b) => a == b,
+				_ => false,
 			}
-		} else {
-			false
+			_ => unreachable!()
 		}
     }
 }
 impl Eq for SearchItem {}
 impl SearchItem {
-	pub fn new<D: Into<u64>>(label: String, mut terms: Vec<String>, mut associations: Vec<SearchAssociation>, timestamp: D) -> SearchItem {
+	pub fn new<D: Into<u64>>(source: SearchItemSource, label: String, mut terms: Vec<String>, timestamp: D) -> SearchItem {
 		terms.shrink_to_fit();
-
-		associations.shrink_to_fit();
-		assert!(!associations.is_empty());
-		associations.sort();
-
 		terms.sort_by(|a, b| a.len().cmp(&b.len()));
 
 		SearchItem {
 			len: terms.iter().map(|x| x.len()).reduce(|a, b| a.max(b)).unwrap_or(0).max(label.len()),
 			label,
 			terms,
-			associations,
 			timestamp: timestamp.into(),
+			source
 		}
 	}
 }
@@ -129,7 +80,7 @@ impl Serialize for SearchItem {
 	{
         let mut tup = serializer.serialize_tuple(2)?;
 		tup.serialize_element(&self.label)?;
-		tup.serialize_element(&self.associations)?;
+		tup.serialize_element(&self.source)?;
 		tup.end()
     }
 }
@@ -141,15 +92,15 @@ impl Searchable for WorkshopItem {
 	fn search_item(&self) -> Option<SearchItem> {
 		let mut terms = self.tags.clone();
 
-		if let Some(steamid64) = &self.steamid64 {
-			terms.push(steamid64.to_owned());
-			// TODO add SteamID32
+		if let Some(steamid) = &self.steamid {
+			terms.push(steamid.raw().to_string());
+			terms.push(steamid.steamid32());
 		}
 
 		Some(SearchItem::new(
+			SearchItemSource::MyWorkshop(self.id),
 			self.title.to_owned(),
 			terms,
-			vec![SearchAssociation::PublishedFileId(self.id)],
 			self.time_updated
 		))
 	}
@@ -173,15 +124,10 @@ impl Searchable for GMAFile {
 			}
 		};
 
-		let mut associations = vec![SearchAssociation::Path(self.path.to_owned())];
-		if let Some(id) = self.id {
-			associations.push(SearchAssociation::PublishedFileId(id));
-		}
-
 		Some(SearchItem::new(
+			SearchItemSource::InstalledAddons(dunce::canonicalize(&self.path).unwrap_or_else(|_| self.path.to_owned())),
 			label,
 			terms,
-			associations,
 			self.modified.and_then(|x| x.duration_since(std::time::SystemTime::UNIX_EPOCH).ok().map(|dur| dur.as_secs())).unwrap_or(0)
 		))
 	}
@@ -203,7 +149,7 @@ unsafe impl Sync for ResultsPtr {}
 pub struct Search {
 	channel: Transaction,
 	dirty: AtomicBool,
-	items: RwLock<Vec<Arc<SearchItem>>>, // TODO store Arc<SearchItem> for less cloning overhead
+	items: RwLock<Vec<Arc<SearchItem>>>,
 	matcher: SkimMatcherV2,
 }
 impl Search {
@@ -235,6 +181,10 @@ impl Search {
 		}
 	}
 
+	pub fn reserve(&self, amount: usize) {
+		self.items.write().reserve(amount);
+	}
+
 	pub fn add_bulk<V: Searchable>(&self, items: &Vec<V>) {
 		self.dirty.store(true, std::sync::atomic::Ordering::Release);
 
@@ -243,13 +193,21 @@ impl Search {
 		store.extend(items.into_iter().filter_map(|v| v.search_item().map(|search_item| Arc::new(search_item))));
 	}
 
-	pub fn quick(&self, query: String) -> Vec<Arc<SearchItem>> {
+	pub fn quick(&self, query: String) -> (Vec<Arc<SearchItem>>, bool) {
 		game_addons!().discover_addons();
+		//steam!().discover_my_workshop_addons();
+		self.dirty();
 
 		let i = AtomicU8::new(0);
-		let mut results: Vec<Option<(i64, Arc<SearchItem>)>> = vec![None; 15];
+		let has_more = AtomicBool::new(false);
+		let mut results: Vec<Option<(i64, Arc<SearchItem>)>> = vec![None; MAX_QUICK_RESULTS as usize];
 
 		self.items.read().par_iter().try_for_each_with(ResultsPtr(&mut results as *mut _), |results, search_item| {
+			if i.load(std::sync::atomic::Ordering::Acquire) >= MAX_QUICK_RESULTS {
+				has_more.store(true, std::sync::atomic::Ordering::Release);
+				return Err(());
+			}
+
 			if search_item.len < query.len() { return Ok(()); }
 
 			let mut winner = None;
@@ -271,7 +229,8 @@ impl Search {
 
 			if let Some(score) = winner {
 				let i = i.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
-				if i >= 15 {
+				if i >= MAX_QUICK_RESULTS {
+					has_more.store(true, std::sync::atomic::Ordering::Release);
 					return Err(());
 				} else {
 					(unsafe { &mut *results.0 })[i as usize] = Some((score, search_item.clone()));
@@ -285,10 +244,12 @@ impl Search {
 		let i = i.into_inner();
 
 		if i == 0 {
-			vec![]
+			(vec![], false)
 		} else if i == 1 {
-			vec![results[0].take().unwrap().1]
+			(vec![results[0].take().unwrap().1], false)
 		} else {
+			let has_more = has_more.load(std::sync::atomic::Ordering::Acquire);
+
 			results.sort_by(|a, b| {
 				if let Some(a) = a {
 					if let Some(b) = b {
@@ -302,12 +263,14 @@ impl Search {
 				return std::cmp::Ordering::Equal;
 			});
 
-			results.into_iter().filter_map(|x| x.map(|x| x.1)).collect()
+			(results.into_iter().filter_map(|x| x.map(|x| x.1)).collect(), has_more)
 		}
 	}
 
 	pub fn full(&'static self, query: String) -> u32 {
 		game_addons!().discover_addons();
+		//steam!().discover_my_workshop_addons();
+		self.dirty();
 
 		let transaction = transaction!();
 		let id = transaction.id;
@@ -342,6 +305,10 @@ impl Search {
 		});
 
 		id
+	}
+
+	pub fn clear(&self) {
+		*self.items.write() = Vec::new();
 	}
 }
 
