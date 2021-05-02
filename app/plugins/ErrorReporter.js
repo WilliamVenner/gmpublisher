91fc2117d496921f321437ea07406f6b9d8394df
William Venner <william@venner.io>
2021-05-02 18:52:28 +0100

    Improve console message reporting

--- app/plugins/ErrorReporter.js
+++ app/plugins/ErrorReporter.js
@@ -22,19 +22,32 @@ console.log('Listening for errors');
 		return stringified;
 	}
 
+	let _error = window.console.error;
+	let _warn = window.console.warn;
+	let _log = window.console.log;
+
 	if (DEBUG_MODE) {
+		_warn = window.console.warn;
+		_log = window.console.log;
+
 		window.console.warn = (...data) => {
 			report('warn', { message: stringify(...data) });
+			_warn(...data);
 		};
 
 		window.console.log = (...data) => {
 			report('info', { message: stringify(...data) });
+			_log(...data);
 		};
 	}
 
-	window.console.error = (...data) => {
-		report('error', { message: stringify(...data) });
-	};
+	{
+		_error = window.console.log;
+		window.console.error = (...data) => {
+			report('error', { message: stringify(...data) });
+			_error(...data);
+		};
+	}
 
 	window.addEventListener('error', e => {
 		report('js_error', {
@@ -42,5 +55,26 @@ console.log('Listening for errors');
 			stack: e.error.stack
 		});
 	});
+
+	window.console.report = (msgType, ...data) => {
+		switch (msgType) {
+			case 'error': {
+				report('error', { message: stringify(...data) });
+				_error(...data);
+				break;
+			}
+
+			case 'warn': {
+				report('warn', { message: stringify(...data) });
+				_warn(...data);
+				break;
+			}
+
+			default: {
+				report('info', { message: stringify(...data) });
+				_log(...data);
+			}
+		}
+	};
 
 })();
