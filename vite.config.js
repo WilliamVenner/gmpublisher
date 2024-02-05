0cd33b08b4e62f1d992e5603fee235d411f49068
William Venner <william@venner.io>
2024-02-05 23:07:15 +0000

    Windows MSI build working

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import replace from '@rollup/plugin-replace';
-import json from '@rollup/plugin-json';
 import { svelteSVG } from "rollup-plugin-svelte-svg";
 import fs from 'fs';
 
@@ -23,41 +22,45 @@ console.log('production', production);
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [svelte()],
+	plugins: [svelte()],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
-  },
-  root: "app",
-  publicDir: "../public",
-  build: {
-    outDir: "./public/build",
-    rollupOptions: {
-      input: {
-        app: './app/index.html',
-      },
-      output: {
-        intro: `window.APP_LANGUAGES = ${JSON.stringify(appLanguages)};`
-      },
-      plugins: [
-        replace({
-          'process.env.NODE_ENV': JSON.stringify(
-            production ? 'production' : 'development'
-          ),
-        }),
-        json(),
-        svelteSVG(),
-      ]
-    },
-  },
+	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+	//
+	// 1. prevent vite from obscuring rust errors
+	clearScreen: false,
+	// 2. tauri expects a fixed port, fail if that port is not available
+	server: {
+		port: 1420,
+		strictPort: true,
+		watch: {
+			// 3. tell vite to ignore watching `src-tauri`
+			ignored: ["**/src-tauri/**"],
+		},
+	},
+	root: "app",
+	publicDir: "../public",
+	build: {
+		inlineDynamicImports: true,
+		outDir: "../dist",
+		rollupOptions: {
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production
+			},
+			input: {
+				app: './app/index.html',
+			},
+			output: {
+				intro: `window.APP_LANGUAGES = ${JSON.stringify(appLanguages)};`
+			},
+			plugins: [
+				replace({
+					'process.env.NODE_ENV': JSON.stringify(
+						production ? 'production' : 'development'
+					),
+				}),
+				svelteSVG(),
+			]
+		},
+	},
 }));
